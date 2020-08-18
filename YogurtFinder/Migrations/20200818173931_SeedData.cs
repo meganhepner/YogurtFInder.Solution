288efe225136231c600894b4e2252572d180a61d
Allison Sadin <aesadin@gmail.com>
2020-08-18 11:38:00 -0700

    add entities, helpers file for authentication with JWT
    co-authored-by: Megan Hepner<megan.hepner@gmail.com>
    Co-authored-by: MarielHamson <mariel.hamson@gmail.com>

---
+++ YogurtFinder/Migrations/20200818173931_SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YogurtFinder.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Yogurts",
+                columns: new[] { "YogurtId", "Blended", "Brand", "Calories", "Carbs", "Fat", "Flavor", "Protein", "Sugar", "Type" },
+                values: new object[,]
+                {
+                    { 1, false, "Ellenos", 360, 38, 17, "key lime", 16, 30, "greek" },
+                    { 2, false, "Brown Cow", 130, 11, 3, "plain", 8, 7, "cream top" },
+                    { 3, true, "Siggi", 140, 11, 5, "coffee", 13, 8, "icelandic" },
+                    { 4, false, "CoCoYo", 90, 8, 6, "vanilla", 1, 2, "dairy free" },
+                    { 5, false, "Chobani Flip", 190, 24, 5, "cookie dough", 11, 21, "dessert, greek" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Yogurts",
+                keyColumn: "YogurtId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Yogurts",
+                keyColumn: "YogurtId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Yogurts",
+                keyColumn: "YogurtId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Yogurts",
+                keyColumn: "YogurtId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Yogurts",
+                keyColumn: "YogurtId",
+                keyValue: 5);
+        }
+    }
+}
--- YogurtFinder/Models/YogurtFinderContext.cs
+++ YogurtFinder/Models/YogurtFinderContext.cs
@@ -20,7 +20,7 @@ namespace YogurtFinder.Models
         new Yogurt { YogurtId = 2, Brand  = "Brown Cow", Flavor = "plain", Blended = false, Type = "cream top", Protein = 8, Sugar = 7, Fat = 3, Carbs = 11, Calories = 130 },
         new Yogurt { YogurtId = 3, Brand  = "Siggi", Flavor = "coffee", Blended = true, Type = "icelandic", Protein = 13, Sugar = 8, Fat = 5, Carbs = 11, Calories = 140 },
         new Yogurt { YogurtId = 4, Brand  = "CoCoYo", Flavor = "vanilla", Blended = false, Type = "dairy free", Protein = 1, Sugar = 2, Fat = 6, Carbs = 8, Calories = 90 },
-        new Yogurt { YogurtId = 5, Brand  = "Chobani Flip", Flavor = "cookie dough", Blended = false, Type = "dessert", "greek", Protein = 11, Sugar = 21, Fat = 5, Carbs = 24, Calories = 190 }
+        new Yogurt { YogurtId = 5, Brand  = "Chobani Flip", Flavor = "cookie dough", Blended = false, Type = "dessert, greek", Protein = 11, Sugar = 21, Fat = 5, Carbs = 24, Calories = 190 }
       );
     }
   }
